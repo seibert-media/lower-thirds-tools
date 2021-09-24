e98abe240bed7a13ec951891e193a25e38d797da
Jenny Danzmayr <mail@evilscientress.at>
2021-09-25 01:35:34 +0200

    mockup für control panel

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,49 @@
         </div>
       </div>
     </section>
+    <section class="columns section is-variable is-6" v-if="currentChannel">
+      <div class="control-panel column">
+        <h4 class="title is-4">Controls</h4>
+          <div class="field">
+            <label class="label">Title</label>
+            <div class="control">
+              <input class="input" type="text" placeholder="Title">
+            </div>
+          </div>
+          <div class="field">
+            <label class="label">Subtitle</label>
+            <div class="control">
+              <input class="input" type="text" placeholder="Subtitle">
+            </div>
+          </div>
+          <div class="field is-grouped">
+          <div class="control">
+            <button class="button is-primary">Go</button>
+          </div>
+          <div class="control">
+            <button class="button is-primary">Preview</button>
+          </div>
+          <div class="control">
+            <button class="button is-danger">Reset</button>
+          </div>
+        </div>
+        <h4 class="title is-4">Styles</h4>
+        <div class="columns is-multiline is-mobile">
+          <div class="column is-one-third" v-for="style in styles">
+            <div class="card">
+              <div class="card-image">
+                <figure class="image is-16by9">
+                  <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                </figure>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="column">
+        <div class="preview"></div>
+      </div>
+    </section>
   </main>
 </template>
 
@@ -37,6 +80,7 @@ export default defineComponent({
     return {
       channels: {} as ChannelsData,
       currentChannel: null as Channel | null,
+      styles: ['test', 'test', 'test', 'test', 'test', 'test',]
     }
   },
   methods: {
